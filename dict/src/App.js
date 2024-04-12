95924825fd7a1b4dca0bcea2eb276af268c75ab2
Logeshwaran29 <logeshwarancse20@gmail.com>
2024-04-13 04:37:34 +0530

    new

--- dict/src/App.js
+++ dict/src/App.js
@@ -1,75 +1,125 @@
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
+import Image from './Images/img.png';
 
 function App() {
     const [word, setWord] = useState('');
     const [definition, setDefinition] = useState('');
     const [parts, setParts] = useState('');
-    const [a, setA] = useState('');
-    const [t, setT] = useState(true);
     const [mean, setMean] = useState('');
-    const [syn, setSyn] = useState('');
+    const [phonetics, setphonetics] = useState([]);
+    const [notImg, setImg] = useState('');
+    const [open, setOpen] = useState(false);
+    const [load, setLoad] = useState(false);
+    const [darkmode, setDarkMode] = useState(false);
 
-    const wordEnter = (e) => {
-        const a = e.target.value;
-        setWord(a);
-    }
-    const change = () => {
-        const a = document.querySelector("#body");
-        const b = document.querySelector("#div");
-        const c = document.querySelector("#b1");
-        if (t) {
-            a.style.backgroundColor = "black";
-            b.style.backgroundColor = "grey";
-            b.style.color = "black";
-            c.style.color = "white";
-            c.style.backgroundColor = "black";
-            c.style.borderColor = "white";
-            setT(false);
-        } else {
-            a.style.backgroundColor = "rgb(148, 146, 146)";
-            b.style.backgroundColor = "black";
-            b.style.color = "white";
-            c.style.color = "black";
-            c.style.backgroundColor = "white";
-            c.style.borderColor = "black";
-            setT(true);
-        }
-    }
+    const styles = {
+        backgroundColor: darkmode ? '#262626' : '#e6e6e6',
+        color: darkmode ? '#ffffff' : '#000000', 
+    };
 
-    const enter = () => {
+    const wordSearch = () => {
+        setLoad(true);
+        setOpen(false);
         axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-            .then((res) => {
+        .then((res) => {
+            setTimeout(() => {
                 const data = res.data[0];
                 setDefinition(data.meanings[0].definitions[0].definition);
-                setParts(data.meanings[0].partOfSpeech);
-                setA("Parts Of Speech: ");
-                setMean("Synonyms: ");
-                setSyn(data.meanings[0].synonyms[0]);
-                console.log(res);
-            })
-            .catch((error) => {
+                setParts(`Parts Of Speech : ${data.meanings[0].partOfSpeech}`);
+                setMean(`Synonyms : ${data.meanings[0].synonyms[0]}`);
+                setphonetics(data.phonetics);
+                setLoad(false); 
+            },2000)
+            console.log(res);
+        })
+        .catch((e) => {
+            setTimeout(() => {
                 setDefinition("OOPS! Word not Found");
-                setParts('');
-                setA('');
-                setMean('');
-                setSyn('');
-            })
+                setImg(Image);
+                setOpen(true);
+                setLoad(false);  
+            }, 2000);
+        })    
+    }
+
+    const playAudio = (audioUrl) => {
+        const audio = new Audio(audioUrl);
+        audio.play();
     }
     return ( 
-    <div className="App">
-      <div><button id="b1" onClick={change}><i class="fa-regular fa-sun"></i></button></div>
-        <div id="div">
-        <h1>DICTIONARY</h1>
-        <input type="text" id="in" placeholder='Enter a word' value={word} onChange={wordEnter} required></input>
-        <button  id="button" onClick={enter}><i class="fa-solid fa-magnifying-glass"></i></button><br/>
-        <div id="d1"><h3>{definition}</h3>
-        <h3>{a}{parts}</h3>
-        <h3>{mean}{syn}</h3>
+        <div className="app" style={styles}>
+            <div className="div">
+            <div className="div1"><h2>DICTIONARY</h2></div>
+            <div className="div2">
+                <div className='input-div'>
+                    <button className="button" onClick={() =>{wordSearch();}} style={{color: darkmode ? 'white' : 'black'}}><i className="fa-solid fa-magnifying-glass"/></button><br/>
+                    <input  
+                        className="input"
+                        type="text" 
+                        placeholder='Enter a word' 
+                        value={word} 
+                        onChange={(e)=>setWord(e.target.value)}
+                        onKeyDown={ (e) =>{
+                            if (e.key === 'Enter') {
+                                wordSearch();  
+                            }
+                        }}
+                        style={{color: darkmode ? 'white' : 'black'}}
+                    />
+                </div>
+                <div className='mode' onClick={(e) => setDarkMode(prevState => !prevState)}>
+                    {darkmode ? (
+                        <i class="fa-solid fa-cloud-moon" />
+                    ):(
+                        <i class="fa-solid fa-cloud-sun" />
+                    )}
+                </div>
+            </div>
+            <div className="div3">
+                {load ? (
+                    <>
+                        <div className='loading'>
+                          <div className="dot"></div>
+                          <div className="dot"></div>
+                          <div className="dot"></div>
+                          <div className="dot"></div>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                    {notImg !=='' && open ? (
+                    <>
+                        <p>{definition}</p>
+                        <img className='img' src={notImg} alt="img" />
+                    </>
+                    ):(
+                        <>
+                            <p>{definition}</p>
+                            <p>{mean}</p>
+                            <p>{parts}</p>
+                            <div className='phonetic'>
+                            {phonetics.map((value, index) => (
+                                value.text !== '' && value.audio !== '' ? (
+                                    <>
+                                    <div className='ph-div' key={index}>
+                                        <p>{`Phonetic : ${value.text}`}</p>
+                                        <i class="fa-solid fa-volume-high" onClick={(e) => playAudio(value.audio)}/>
+                                    </div>
+                                    </>
+                                ):(
+                                    <></>
+                                )
+                            ))}
+                            </div>
+                        </>
+                    )}
+                    </>
+                )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
     );
 }
 
